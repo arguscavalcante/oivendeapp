43c4fad184fd1ad24e659cb88bc900bca9f95775
Vanildo Vanni <vanildo.vanni@gmail.com>
2015-09-11 15:53:56 -0300

    teste

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('OiVendeApp', ['ionic', 'pouchdb','lbServices'])
+angular.module('OiVendeApp', ['ionic', 'pouchdb','lbServices','angular-jquery-maskedinput'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers/TelefoneCtrl.js
+++ www/js/controllers/TelefoneCtrl.js
@@ -6,35 +6,37 @@ app.controller('TelefoneCtrl', function($scope, $state, Cliente, Porta) {
     $scope.cliente = {};
     $scope.portas = [];
     $scope.exibirDadosCliente = false;
+    $scope.exibirConsultaTelefone = true;
+    $scope.msgConsultaTelefone = "";
 
     $scope.consultarTelefone = function(tel) {
-        var telefone = tel.ddd + tel.telefone;
-        var cpf = tel.cpf;
-        console.log("Dados: " + telefone + " - " + cpf);
-        Cliente.find({filter:
-                        {where: {
-                            telefone: telefone
-                     }}})
+        console.log(JSON.stringify(tel));
+        $scope.msgConsultaTelefone = "";
+        var telefone = tel.ddd.replace('(','').replace(')', '') + tel.telefone.replace('(','').replace('-', '') ;
+        var cpf = tel.cpf.split('.').join('').replace('-','');
+        Cliente.find({filter: { where: { telefone: telefone }}})
                .$promise
                .then(function(result){
                    console.log("Cliente encontrado: ");
                    console.log(JSON.stringify(result));
                    $scope.cliente = result[0];
-                   Porta.find({filter:
-                                   {where: {
-                                       telefone: telefone
-                                }}})
+                   Porta.find({filter: {where: { telefone: telefone }}})
                         .$promise
                         .then(function(result) {
                             console.log("Portas encontradas: ");
                             console.log(JSON.stringify(result));
                             $scope.portas = result;
                             // $state.go("app.velocidade", {cliente: $scope.cliente, portas: $scope.portas});
+                            $scope.exibirConsultaTelefone = false;
                             $scope.exibirDadosCliente = true;
+                            if ($scope.cliente == null) {
+                                $scope.msgConsultaTelefone = "Cliente não encontrado";
+                            } else {
+
+                            }
                         });
         });
     }
-
 
     $scope.$on('$ionicView.enter', function(e) {
 
